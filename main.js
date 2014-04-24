6e2c1fbaa12b1c8a69651d8ca819e5dfb423aad1
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2014-04-24 11:03:19 -0500

    Not quite working

--- main.js
+++ main.js
@@ -31,19 +31,29 @@ function main() {
 	var program_calc = createProgram(
 		document.getElementById('calc-vs').text,
 		document.getElementById('calc-fs').text);
+	var program_slvr = createProgram(
+		document.getElementById('slvr-vs').text,
+		document.getElementById('slvr-fs').text);
 	var program_draw = createProgram(
 		document.getElementById('draw-vs').text,
 		document.getElementById('draw-fs').text);
 
 	// Uniforms
-	program_calc.u_dt		= gl.getUniformLocation(program_phys, 'u_dt');
+	program_phys.u_dt		= gl.getUniformLocation(program_phys, 'u_dt');
+	program_calc.u_dt		= gl.getUniformLocation(program_calc, 'u_dt');
 	program_phys.u_viewport	= gl.getUniformLocation(program_phys, 'u_viewport');
 	program_calc.u_viewport	= gl.getUniformLocation(program_calc, 'u_viewport');
+	program_slvr.u_viewport	= gl.getUniformLocation(program_slvr, 'u_viewport');
 
 	program_phys.u_state	= gl.getUniformLocation(program_phys, 'u_state');
 	program_calc.u_state	= gl.getUniformLocation(program_calc, 'u_state');
+	program_phys.u_dot		= gl.getUniformLocation(program_phys, 'u_dot');
 	program_calc.u_dot		= gl.getUniformLocation(program_calc, 'u_dot');
 	program_draw.u_state	= gl.getUniformLocation(program_draw, 'u_state');
+	program_slvr.u_dot1		= gl.getUniformLocation(program_slvr, 'u_dot1');
+	program_slvr.u_dot2		= gl.getUniformLocation(program_slvr, 'u_dot2');
+	program_slvr.u_dot3		= gl.getUniformLocation(program_slvr, 'u_dot3');
+	program_slvr.u_dot4		= gl.getUniformLocation(program_slvr, 'u_dot4');
 
 	program_draw.u_vp		= gl.getUniformLocation(program_draw, 'u_vp');
 
@@ -55,6 +65,7 @@ function main() {
 	// Attributes
 	program_phys.a_rectangle = gl.getAttribLocation(program_phys, 'a_rectangle');
 	program_calc.a_rectancle = gl.getAttribLocation(program_calc, 'a_rectangle');
+	program_slvr.a_rectancle = gl.getAttribLocation(program_slvr, 'a_rectangle');
 	program_draw.a_reference = gl.getAttribLocation(program_draw, 'a_reference');
 
 	var initial_state = new Float32Array(4 * NUM_PARTICLES * NUM_SLOTS);
@@ -63,9 +74,9 @@ function main() {
 		initial_state[i * 8 + 0] = (Math.random() - 0.5) * 2.0;
 		initial_state[i * 8 + 1] = (Math.random() - 0.5) * 2.0;
 		initial_state[i * 8 + 2] = (Math.random() - 0.5) * 2.0;
-		initial_state[i * 8 + 4] = (Math.random() - 0.5) * 0.01;
-		initial_state[i * 8 + 5] = (Math.random() - 0.5) * 0.01;
-		initial_state[i * 8 + 6] = (Math.random() - 0.5) * 0.01;
+		initial_state[i * 8 + 4] = (Math.random() - 0.5);
+		initial_state[i * 8 + 5] = (Math.random() - 0.5);
+		initial_state[i * 8 + 6] = (Math.random() - 0.5);
 	}
 
 	// Camera
@@ -88,26 +99,70 @@ function main() {
 
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, STATE_TEXTURE_WIDTH, STATE_TEXTURE_HEIGHT, 0, gl.RGBA, gl.FLOAT, initial_state);
 
-	gl.useProgram(program_phys);
-	gl.uniform1i(program_phys.u_state, 0);
-	gl.useProgram(program_calc);
-	gl.uniform1i(program_calc.u_state, 0);
 	gl.useProgram(program_draw);
 	gl.uniform1i(program_draw.u_state, 0);
+	gl.useProgram(program_calc);
+	gl.uniform1i(program_calc.u_state, 0);
 
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-	var texture_dot = gl.createTexture();
+	var texture_dot1 = gl.createTexture();
 	gl.activeTexture(gl.TEXTURE1);
-	gl.bindTexture(gl.TEXTURE_2D, texture_dot);
+	gl.bindTexture(gl.TEXTURE_2D, texture_dot1);
 
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, STATE_TEXTURE_WIDTH, STATE_TEXTURE_HEIGHT, 0, gl.RGBA, gl.FLOAT, initial_state);
 
+	gl.useProgram(program_phys);
+	gl.uniform1i(program_phys.u_dot, 1);
 	gl.useProgram(program_calc);
 	gl.uniform1i(program_calc.u_dot, 1);
+	gl.useProgram(program_slvr);
+	gl.uniform1i(program_slvr.u_dot1, 1);
+
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+	var texture_dot2 = gl.createTexture();
+	gl.activeTexture(gl.TEXTURE2);
+	gl.bindTexture(gl.TEXTURE_2D, texture_dot2);
+
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, STATE_TEXTURE_WIDTH, STATE_TEXTURE_HEIGHT, 0, gl.RGBA, gl.FLOAT, initial_state);
+
+	gl.useProgram(program_slvr);
+	gl.uniform1i(program_slvr.u_dot2, 2);
+
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+	var texture_dot3 = gl.createTexture();
+	gl.activeTexture(gl.TEXTURE3);
+	gl.bindTexture(gl.TEXTURE_2D, texture_dot3);
+
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, STATE_TEXTURE_WIDTH, STATE_TEXTURE_HEIGHT, 0, gl.RGBA, gl.FLOAT, initial_state);
+
+	gl.useProgram(program_slvr);
+	gl.uniform1i(program_slvr.u_dot3, 3);
+
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+	var texture_dot4 = gl.createTexture();
+	gl.activeTexture(gl.TEXTURE4);
+	gl.bindTexture(gl.TEXTURE_2D, texture_dot4);
+
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, STATE_TEXTURE_WIDTH, STATE_TEXTURE_HEIGHT, 0, gl.RGBA, gl.FLOAT, initial_state);
+
+	gl.useProgram(program_slvr);
+	gl.uniform1i(program_calc.u_dot4, 4);
 
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
@@ -119,9 +174,21 @@ function main() {
 	gl.bindFramebuffer(gl.FRAMEBUFFER, fb_state);
 	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture_state, 0);
 
-	var fb_dot = gl.createFramebuffer();
-	gl.bindFramebuffer(gl.FRAMEBUFFER, fb_dot);
-	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture_dot, 0);
+	var fb_dot1 = gl.createFramebuffer();
+	gl.bindFramebuffer(gl.FRAMEBUFFER, fb_dot1);
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture_dot1, 0);
+
+	var fb_dot2 = gl.createFramebuffer();
+	gl.bindFramebuffer(gl.FRAMEBUFFER, fb_dot2);
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture_dot2, 0);
+
+	var fb_dot3 = gl.createFramebuffer();
+	gl.bindFramebuffer(gl.FRAMEBUFFER, fb_dot3);
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture_dot3, 0);
+
+	var fb_dot4 = gl.createFramebuffer();
+	gl.bindFramebuffer(gl.FRAMEBUFFER, fb_dot4);
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture_dot4, 0);
 
 	var reference	= new Float32Array(NUM_PARTICLES * 2);
 	var interval	= 1.0 / PARTICLES_PER_ROW;
@@ -147,9 +214,25 @@ function main() {
 	]), gl.STATIC_DRAW);
 	gl.enableVertexAttribArray(program_phys.a_rectangle);
 	gl.enableVertexAttribArray(program_calc.a_rectangle);
+	gl.enableVertexAttribArray(program_slvr.a_rectangle);
 
 	gl.useProgram(program_draw);
 	gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+	var solve = function(source_dot_id, target_dot, dt) {
+		gl.useProgram(program_phys);
+		gl.uniform1i(program_phys.u_dot, source_dot_id);
+		gl.uniform1f(program_phys.u_dt, dt);
+
+		// PHYSICS
+		gl.bindBuffer(gl.ARRAY_BUFFER, buffer_rectangle);
+		gl.vertexAttribPointer(program_phys.a_rectangle, 2, gl.FLOAT, gl.FALSE, 0, 0);
+
+		gl.bindFramebuffer(gl.FRAMEBUFFER, target_dot);
+		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+	}
+
+	var mode = 1;
 
 	var last = Date.now();
 
@@ -160,17 +243,28 @@ function main() {
 
 		stats.begin();
 
-		// PHYSICS
 		gl.viewport(0, 0, STATE_TEXTURE_WIDTH, STATE_TEXTURE_HEIGHT);
-		gl.useProgram(program_phys);
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffer_rectangle);
-		gl.vertexAttribPointer(program_phys.a_rectangle, 2, gl.FLOAT, gl.FALSE, 0, 0);
 
-		gl.bindFramebuffer(gl.FRAMEBUFFER, fb_dot);
-		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+		if (mode === 0) {
+			//EULER
+			solve(1, fb_dot1, 0.0);
+		} else {
+			//RUNGE KUTTA
+			solve(1, fb_dot1, 0.0);
+			solve(1, fb_dot2, dt / 2.0);
+			solve(2, fb_dot3, dt / 2.0);
+			solve(3, fb_dot4, dt);
+
+			gl.useProgram(program_slvr);
+
+			gl.bindBuffer(gl.ARRAY_BUFFER, buffer_rectangle);
+			gl.vertexAttribPointer(program_slvr.a_rectangle, 2, gl.FLOAT, gl.FALSE, 0, 0);
+
+			gl.bindFramebuffer(gl.FRAMEBUFFER, fb_dot1);
+			gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+		}
 
 		// CALCULATION
-		gl.viewport(0, 0, STATE_TEXTURE_WIDTH, STATE_TEXTURE_HEIGHT);
 		gl.useProgram(program_calc);
 		gl.bindBuffer(gl.ARRAY_BUFFER, buffer_rectangle);
 		gl.vertexAttribPointer(program_calc.a_rectangle, 2, gl.FLOAT, gl.FALSE, 0, 0);
